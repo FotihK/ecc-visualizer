d2c76b66d39652684ea885dc8b648d0092982194
Fotih Karimov <19657828+FotihK@users.noreply.github.com>
2025-12-02 21:42:54 -0500

    Working version

---
+++ d3.js
@@ -0,0 +1,402 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import * as field from './fields.js'
+
+"use strict";
+
+const width = 700;
+const height = 600;
+
+const margin = { top: 40, bottom: 36, right: 16, left: 24 };
+const innerW = width - margin.left - margin.right;
+const innerH = height - margin.top - margin.bottom;
+
+const maxTicks = 35;
+
+const tooltip = d3.select("#tooltip");
+
+const svg = d3.select("#svg")
+  .attr("width", width)
+  .attr("height", height);
+
+const graph = svg.append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`)
+  .attr('clip-path', 'url(#surround)');
+
+// graph.append('rect')
+//   .attr('x', -30)
+//   .attr('y', -8)
+//   .attr('width', width - 2)
+//   .attr('height', height - 10)
+//   .attr('fill', 'pink');
+
+const gx = graph.append("g")
+  .attr("id", 'x-axis')
+  .attr("transform", `translate(0,${innerH})`)
+const gy = graph.append("g")
+  .attr('id', 'y-axis')
+  .attr("transform", `translate(0,0)`)
+
+const lines = graph.append("g")
+  .attr('id', 'lines');
+
+const ptsGroup = graph.append("g")
+  .attr("id", "points")
+
+const eqn = svg.append("g")
+  .attr("transform", `translate(${margin.left},${margin.top / 2})`)
+  .append("text")
+  .classed('typeset', true)
+  .attr('id', 'eqn')
+  .attr("x", 6).attr("y", 6)
+  .attr("font-size", 12)
+  .text('');
+
+const xs = d3.scaleLinear().range([0, innerW]);
+const ys = d3.scaleLinear().range([innerH, 0]);
+
+let last = { a: -1, b: -1, p: -1 };
+
+function changed(a, b, p) {
+  if (last.a === a && last.b === b && last.p === p) return false;
+  last = { a, b, p };
+  return true;
+}
+
+function init(p) {
+  const t = d3.transition().duration(300).ease(d3.easeExp);
+  const tr = e => e.transition(t);
+  t.on('end', () => button.removeAttribute('disabled'));
+  gx.selectAll('*').interrupt();
+  gy.selectAll('*').interrupt();
+  ptsGroup.selectAll('*').interrupt();
+
+  button.setAttribute("disabled", "");
+  selectPoint(null);
+
+  xs.domain([0, p - 1]);
+  ys.domain([0, p - 1]);
+  const ticks = Math.min(p - 1, maxTicks);
+  const xAxis = d3.axisBottom(xs).ticks(ticks);
+  const yAxis = d3.axisLeft(ys).ticks(ticks);
+
+  return { tr, ticks, xAxis, yAxis };
+}
+
+function drawAxes(tr, xAxis, yAxis) {
+  tr(gx)
+    .call(xAxis)
+    .selectAll(".tick:not(:first-of-type) line")
+    .attr('class', 'grid-line')
+    .attr('y1', -1)
+    .attr('y2', -1 * innerH);
+
+  tr(gy)
+    .call(yAxis)
+    .selectAll(".tick:not(:first-of-type) line")
+    .attr('class', 'grid-line')
+    .attr('x1', 1)
+    .attr('x2', innerW);
+}
+
+let points;
+let scale;
+
+const notes = d3.select('#notes');
+
+function drawField(a, b, p) {
+  if (!changed(a, b, p)) return;
+  console.clear();
+
+  const { tr, ticks, xAxis, yAxis } = init(p);
+  drawAxes(tr, xAxis, yAxis);
+
+  field.init(a, b, p);
+  if (!field.isPrime()) notes.classed('hidden', false).text('p is not prime!');
+  else if (!field.isSingular()) notes.classed('hidden', false).text('this field is non-singular!');
+  else notes.classed('hidden', true).text('');
+  points = field.points().slice(1);
+  scale = ticks / (field.p() - 1);
+
+  ptsGroup.selectAll("circle.point")
+    .data(points, pt => pt.toString())
+    .join(
+      enter => enter.append('circle')
+        .classed('point', true)
+        .attr('r', 0)
+        .attr('cx', pt => xs(pt.x))
+        .attr('cy', pt => ys(pt.y))
+        .call(e => tr(e).attr('r', 5 * scale)),
+      u => tr(u).attr('cx', d => xs(d.x)).attr('cy', d => ys(d.y)).attr('r', 5 * scale),
+      e => tr(e).attr('r', 0).remove())
+    .on('click', e => selectPoint(e.currentTarget))
+    .on('contextmenu', (e, pt) => { e.preventDefault(); addPoint(selectedPoint?.datum(), pt) })
+    .call(mouseEvents);
+
+  eqn.text(`y² \u2261 x³ ${coeff(a, 'x')} ${coeff(b, '')} (mod ${p})`);
+}
+
+const mouseEvents = el => el.on("mouseover.ttip", (evt, pt) =>
+  tooltip.style("visibility", "visible")
+    .style('opacity', '100')
+    .call(ttip, d3.pointer(evt, document.body))
+    .text(pt))
+  .on("mousemove.ttip", evt => tooltip.call(ttip, d3.pointer(evt, document.body)))
+  .on("mouseout.ttip", () => tooltip.style("visibility", 'hidden').style('opacity', '0'))
+
+const ttip = (t, [mx, my]) => t.style("left", `${mx + 8}px`).style("top", `${my - 4}px`)
+function coeff(co, term) {
+  if (co === 0) return "";
+  if (co === 1) return `+ ${term || co}`;
+  if (co === -1) return `- ${term || Math.abs(co)}`;
+  if (co > 1) return `+ ${co}${term}`;
+  return `- ${Math.abs(co)}${term}`;
+}
+
+const topbox = d3.select('#topbox');
+const active = d3.select('#activebox');
+const subgroup = d3.select('#subgroup');
+const addbox = d3.select('#addbox');
+let hr = d3.select('hr');
+let selectedPoint = undefined;
+
+function selectPoint(target = null) {
+  MathJax.typesetClear([addbox.node(), active.node(), subgroup.node()]);
+  topbox.classed('hidden', true);
+  active.text('');
+  subgroup.html('');
+  addbox.text('');
+  lines.selectAll('*').interrupt().remove();
+  hr.classed('hidden', true);
+  clearSums();
+  selectedPoint?.classed('active', false);
+
+  if (!target) {
+    selectedPoint = null;
+    return;
+  }
+
+  selectedPoint = d3.select(target);
+  selectedPoint.classed('active', true);
+
+  const group = Array.from(selectedPoint.datum().generate());
+  const groupstr = `\\(${group.join('\\to')}\\)`
+  // console.log(groupstr);
+  topbox.classed('hidden', false);
+  active.text(`Selected \\(P=${selectedPoint.datum().toString()}\\)`);
+  subgroup.html(`Cyclic group generated by \\(P\\,(|G|=${group.length})\\): <br>${groupstr}`)
+  MathJax.typesetPromise([active.node(), subgroup.node()]);
+}
+
+let sumPoint, negSumPoint;
+const clearSums = () => {
+  sumPoint?.classed('sum', false);
+  negSumPoint?.classed('negative', false);
+};
+
+async function addPoint(P, Q, write = true) {
+  if (!P || !Q) return;
+  clearSums();
+  const sum = P.plus(Q);
+  if (write) {
+    hr.classed('hidden', false);
+    MathJax.typesetClear([addbox.node()]);
+    addbox.text(`\\(${P} + ${Q} = ${sum}\\)`)
+    MathJax.typesetPromise([addbox.node()]);
+  }
+
+  lines.selectAll('*').interrupt().remove();
+
+  if (P.isInfinity()) return Q;
+  else if (Q.isInfinity()) return P;
+
+  lines.append('line')
+    .attr('x1', xs(P.x))
+    .attr('y1', ys(P.y))
+    .attr('x2', xs(P.x))
+    .attr('y2', ys(P.y))
+    .attr('stroke', 'orange')
+    .attr('stroke-width', Math.min(2, 5 * scale))
+    .attr('stroke-dasharray', '8 4')
+    .transition()
+    .ease(d3.easeLinear)
+    .duration(1000)
+    .attr('x2', xs(Q.x))
+    .attr('y2', ys(Q.y));
+
+  if (sum.isInfinity()) {
+    lines.append('line')
+      .attr('x1', xs(Q.x))
+      .attr('y1', ys(Q.y))
+      .attr('x2', xs(Q.x))
+      .attr('y2', ys(Q.y))
+      .attr('stroke', 'red')
+      .attr('stroke-width', Math.min(2, 4 * scale))
+      .transition()
+      .ease(d3.easeLinear)
+      .delay(1000)
+      .duration(500)
+      .attr('y1', ys(0))
+      .attr('y2', ys(field.p() - 1))
+    return sum;
+  }
+
+  const eps = 1e-5;
+  const negSum = sum.negate();
+  let curr = Q.asObj();
+  const m = slope(P, Q);
+  const ends = [];
+  const rev = P.x > Q.x;
+  let end;
+
+  // console.log(P, Q, m);
+
+  sumPoint = ptsGroup.selectAll('circle').filter(d => d.equals(sum));
+  negSumPoint = ptsGroup.selectAll('circle').filter(d => d.equals(negSum));
+
+  const min = 0;
+  const maxim = field.p();
+  let flag = Q.equals(negSum);
+
+  for (let i = 0; i < 100; i++) {
+    let xP = rev ? min - curr.x : maxim - curr.x;
+    let yc = curr.y + (m * xP);
+    if (yc < (min + eps)) xP = xAtY(curr, m, min) - curr.x;
+    else if (yc > (maxim - eps)) xP = xAtY(curr, m, maxim) - curr.x;
+    end = { x: curr.x + xP, y: curr.y + (m * xP) };
+
+    if (!flag && inline(curr, end, negSum)) {
+      // console.log(`inline after ${i}`, curr, end)
+      end = { x: negSum.x, y: negSum.y };
+      ends.push([curr, end]);
+      break;
+    }
+    flag = false;
+    ends.push([curr, end]);
+
+    // console.log(curr, end);
+    let { x: nx, y: ny } = end;
+    if (nx < (min + eps) || nx >= maxim - eps) nx = maxim - nx;
+    if (ny < (min + eps) || ny >= maxim - eps) ny = maxim - ny;
+    curr = { x: nx, y: ny };
+  }
+
+  try {
+    await lines.selectAll('.addLine')
+      .data(ends)
+      .join('line')
+      .classed('addLine', true)
+      .attr('x1', d => xs(d[0].x))
+      .attr('y1', d => ys(d[0].y))
+      .attr('x2', d => xs(d[0].x))
+      .attr('y2', d => ys(d[0].y))
+      .transition()
+      .ease(d3.easeQuad)
+      .delay((d, i) => 1000 + 250 * i)
+      .duration(200)
+      .attr('x2', d => xs(d[1].x))
+      .attr('y2', d => ys(d[1].y))
+      .end();
+  } catch (_) {
+    return sum;
+  }
+
+  if (!sum.equals(negSum)) {
+    negSumPoint.classed('negative', true);
+
+    try {
+      await lines.append('line')
+        .attr('x1', xs(negSum.x))
+        .attr('y1', ys(negSum.y))
+        .attr('x2', xs(negSum.x))
+        .attr('y2', ys(negSum.y))
+        .attr('stroke', 'red')
+        .attr('stroke-width', Math.min(2, 4 * scale))
+        .attr('stroke-dasharray', '4 2')
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(1000)
+        .attr('x2', xs(sum.x))
+        .attr('y2', ys(sum.y))
+        .end();
+    } catch (_) {
+      return sum;
+    }
+  }
+
+  sumPoint.classed('sum', true);
+  return sum;
+}
+
+async function multiply(P, n) {
+  if (!P || n < 2) return;
+  hr.classed('hidden', false);
+  const nP = P.times(n);
+  clearSums();
+  const bits = n.toString(2).substring(1);
+  let res = P;
+  console.log(bits);
+  let str = `${n}P &= ${nP}:`
+  const typeset = () => {
+    MathJax.typesetClear([addbox.node()]);
+    addbox.text(`\\(\\begin{align*}${str}\\end{align*}\\)`);
+    MathJax.typesetPromise([addbox.node()]);
+  };
+  typeset();
+  let k = 1;
+  for (const bit of bits) {
+    str += `\\\\ ${2 * k}P &= ${k === 1 ? '' : k}P + ${k === 1 ? '' : k}P = ${res} + ${res} = ${res.plus(res)}`
+    k *= 2;
+    typeset();
+    res = await addPoint(res, res, false);
+    await new Promise(r => setTimeout(r, 2000));
+    if (bit === '1') {
+      str += `\\\\ ${k + 1}P &= ${k === 1 ? '' : k}P + P = ${res} + ${P} = ${res.plus(P)}`
+      k += 1;
+      typeset();
+      res = await addPoint(res, P, false);
+      await new Promise(r => setTimeout(r, 2000));
+    }
+  }
+}
+
+function xAtY(p, m, y) {
+  let c = p.y - m * p.x;
+  return (y - c) / m;
+}
+
+function slope(P, Q) {
+  if (P.equals(Q)) return field.slope(P, P);
+  else if (P.x === Q.x) return +Infinity;
+  else return (Q.y - P.y) / (Q.x - P.x);
+}
+
+function dist(P, Q) {
+  return Math.sqrt((P.x - Q.x) ** 2 + (P.y - Q.y) ** 2);
+}
+
+function inline(A, B, C) {
+  return dist(A, C) + dist(B, C) - dist(A, B) <= 1e-9;
+}
+
+function fEq(x, y) {
+  return Math.abs(x - y) <= 1e-6;
+}
+
+const inputs = ['a', 'b', 'p'].map(id => document.getElementById(id));
+const button = document.getElementById("draw")
+const getInput = () => inputs.map(input => Number(input.value));
+button.addEventListener("click", () => drawField(...getInput()));
+
+inputs.forEach(input =>
+  input.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      button.click();
+    }
+  })
+);
+
+const kInput = document.getElementById('k');
+document.getElementById("mult").addEventListener('click', () => multiply(selectedPoint?.datum(), Number(kInput.value)));
+
+button.click();
---
+++ fields.js
@@ -0,0 +1,139 @@
+"use strict";
+const primes = new Set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+  73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+  179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271]);
+
+const params = {
+  a: 2,
+  b: 3,
+  p: 7
+};
+
+const cache = {
+  sqrt: [],
+  inv: [],
+  points: []
+};
+
+const a = () => params.a;
+const b = () => params.b;
+const p = () => params.p;
+
+const reduce = (x) => ((x % params.p) + params.p) % params.p;
+const inverse = (x) => cache.inv[reduce(x)];
+const negate = (x) => reduce(params.p - x);
+
+const isPrime = () => primes.has(params.p);
+const isSingular = () => reduce((4 * params.a * params.a * params.a) + (27 * params.b * params.b)) !== 0;
+const points = () => cache.points;
+const find = (x, y) => cache.points.find(p => p.x === x && p.y === y)
+
+function slope(P, Q) {
+  if (P.equals(Q)) return reduce(reduce(3 * P.x * P.x + params.a) * inverse(2 * P.y));
+  if (P.x === Q.x) return +Infinity;
+  return reduce((Q.y - P.y) * inverse(Q.x - P.x));
+}
+
+
+
+function init(a = 2, b = 3, p = 7) {
+  params.a = a;
+  params.b = b;
+  params.p = p;
+
+  cache.sqrt = Array(p).fill(-1);
+  cache.inv = Array(p);
+  cache.points = [];
+
+  for (let i = 0; i < p; i++) {
+    if (i <= p / 2) cache.sqrt[i * i % p] = i;
+    for (let j = i; j < p; j++) {
+      if (i * j % p === 1) {
+        cache.inv[i] = j;
+        cache.inv[j] = i;
+        break;
+      }
+    }
+  }
+
+  cache.points.push(new pt());
+
+  for (let i = 0; i < p; i++) {
+    for (let j = 0; j <= p / 2; j++) {
+      if (j * j % p === reduce(i * i * i + a * i + b)) {
+        cache.points.push(new pt(i, j));
+        if (j % p !== (p - j) % p) {
+          cache.points.push(new pt(i, p - j));
+        }
+      }
+    }
+  }
+}
+
+class pt {
+  x;
+  y;
+
+  constructor(x = -1, y = -1) {
+    this.x = x;
+    this.y = y;
+  }
+
+  copy() {
+    return new pt(this.x, this.y);
+  }
+
+  asObj() {
+    return {x: this.x, y: this.y};
+  }
+
+  isInfinity() {
+    return (this.x === -1) && (this.y === -1);
+  }
+
+  isNaN() {
+    return isNaN(this.x) || isNaN(this.y);
+  }
+
+  toString() {
+    return this.isInfinity() ? '\\mathcal{O}' : `(${this.x}, ${this.y})`;
+  }
+
+  equals(other) {
+    return this.x === other.x && this.y === other.y;
+  }
+
+  plus(other) {
+    if (this.isInfinity()) return other;
+    if (other.isInfinity()) return this;
+
+    let s = slope(this, other)
+    if (!isFinite(s)) return new pt();
+
+    const nx = reduce(s * s - this.x - other.x);
+    const ny = reduce(s * (this.x - nx) - this.y);
+    return new pt(nx, ny);
+  }
+
+  times(n) {
+    if (n === 0) return new pt();
+    if (n === 1) return this;
+    if (n % 2 == 1) return this.plus(this.times(n - 1));
+    return this.plus(this).times(n / 2);
+  }
+
+  negate() {
+    return new pt(this.x, negate(this.y));
+  }
+
+  *generate() {
+    let next = this;
+    do {
+      yield next;
+      next = next.plus(this);
+    } while (!next.isNaN() && !next.equals(this));
+    return;
+  }
+}
+
+export { a, b, p, params, reduce, negate, isPrime, isSingular, points, find, slope, init, pt };
